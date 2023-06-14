3238eef265bda53efa4f4fd05732f1c5e1294b9d
TheUrbanPenguin <andrew.mallett@theurbanpenguin.com>
2023-06-14 13:41:28 +0100

    Add

--- email.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-
-email = input("Enter an email address: ")
-at = email.find('@')
-user = email[:at]
-domain = email[at + 1:]
-print(f'User: {user}')
-print(f'Domain: {domain}')
-
